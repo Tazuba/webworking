b24b7051f8a14b29d2fdd66eefc752ab5aa9a9cf
tazubac <tazubac@gmail.com>
2020-09-17 19:13:14 +0300

    first commit

---
+++ comingsoon.php
@@ -0,0 +1,55 @@
+<?php
+/* Template Name: comingsoon */
+
+include('header.php');
+?>				
+			<section class="page_title ls s-py-50 corner-title ls invise overflow-visible">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h1>Coming Soon</h1>
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item">
+									<a href="./">Home</a>
+								</li>
+								<li class="breadcrumb-item">
+									<a href="#">Pages</a>
+								</li>
+								<li class="breadcrumb-item active">
+									Coming Soon
+								</li>
+							</ol>
+							<div class="divider-15 d-none d-xl-block"></div>
+						</div>
+                    </div>
+                    
+				</div>
+			</section>
+
+
+			<section class="ls s-pt-10 s-pb-130 s-py-lg-130 page_title comingsoon ls s-overlay">
+            <div class="d-none d-lg-block divider-70"></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center content-soon">
+							<h2 class="special-heading">
+								Coming Soon!
+							</h2>
+							<p>Stay Tuned!</p>
+
+							<div id="comingsoon-countdown"></div>
+							<!--
+					use "data-date" attribute with your date value to set date that you need to count to
+					<div id="comingsoon-countdown" data-date="March 29, 2018 10:00:00"></div>
+				-->
+
+						</div>
+
+						<div class="d-none d-lg-block divider-120"></div>
+					</div>
+
+				</div>
+			</section>
+
+
+			<?php include('footer.php');?>
---
+++ web-apps.php
@@ -0,0 +1,168 @@
+<?php include('header.php');?>
+
+
+
+			<div class="service-full">
+				<img src="images/service/app2.png" alt="" width = "100%">
+			</div>
+
+			<section class="ls" style = "background-image:url('images/web/bg.svg'); color:white;">
+				<div class="container ">
+					<div class="row c-gutter-60">
+						<div class="col-md-8 single-page">
+							<div class="item-content">
+								<p class="excerpt">
+								Advanced Web Technologies
+								</p>
+								<p>
+								Responsive web design allows for optimal viewing of sites across multiple platforms, from mobile to tablet to desktops. This approach minimizes the resizing and scrolling required and adapts to your device, delivering an enhanced user experience.
+
+								</p>
+								<p>
+								Backed by extensive experience in advanced web technologies, our core website development services allow us to create powerful dynamic websites that built on the latest technologies. Having worked across various industries, our expertise spread across various business markets and domains.
+
+								Our work process allows us to iterate from scratch, develop through experience, enhance through innovation and deploy only best in class websites. 
+								</p>
+								<div class = "row">
+								<div class = "col-6">
+									<p>
+									To date, we have successfully designed, deployed, maintained and grown numerous business websites across a wide a varied business landscape.
+
+Providing website solutions for:
+
+    eCommerce Shops
+    Responsive Websites for Tourism
+    Content Managed Corporate Websites
+    Product Catalog Websites
+    Entertainment Website Portals
+    Technology Focused Websites
+									</p>
+								</div>
+								<!--colum two-->
+								<div class = "col-6" style = "background-image:url('images/service/mobile.png'); background-size:cover;">
+								</div>
+								</div><!--close row-->
+																<!-- <p class="text">
+									At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+									magna aliquyam erat, sed diam voluptua at vero eos et accusam et justo duo dolores et ea rebum.
+								</p> -->
+								<div class="d-none d-lg-block divider-40"></div>
+							</div>
+						</div>
+						<!-- .col-* -->
+
+						<div class="col-md-4 hero-bg widget-service">
+							<h6>Other Services</h6>
+
+							<ul class="list">
+								<li>
+									<a href="services1.html">Business Apps</a>
+								</li>
+								<li>
+									<a href="services1.html">UI&UX Design</a>
+								</li>
+								<li>
+									<a href="services1.html">Mobile Apps</a>
+								</li>
+								<li>
+									<a href="services1.html">Database Development</a>
+								</li>
+								<li>
+									<a href="services1.html">Branding</a>
+								</li>
+								<li>
+									<a href="services1.html">Ecommerce</a>
+								</li>
+								<li>
+									<a href="services1.html">Online Marketing</a>
+								</li>
+								<li>
+									<a href="services1.html">Print Design</a>
+								</li>
+								<li>
+									<a href="services1.html">Website Design</a>
+								</li>
+								<li>
+									<a href="services1.html">Corporate Identity</a>
+								</li>
+								<li>
+									<a href="services1.html">SMM</a>
+								</li>
+								<li>
+									<a href="services1.html">SEO</a>
+								</li>
+								<li>
+									<a href="services1.html">Case Studios</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section_testimonials" class="s-pt-30 s-pb-30 s-pt-lg-50 s-pb-lg-100 s-parallax testimonials-sliders ds">
+				<div class="container">
+					<div class="row">
+						<div class="divider-50 d-none d-lg-block"></div>
+						<div class="col-md-12">
+							<div class="owl-carousel testimonials-owl" data-autoplay="false" data-responsive-lg="1" data-responsive-md="1" data-responsive-sm="1" data-nav="false" data-dots="true">
+								<div class="quote-item">
+									<div class="quote-image">
+										<img src="images/team/02.jpg" alt="">
+									</div>
+									<p class="small-text author-job">
+										Eye Insurance
+									</p>
+									<h5>
+										George M. Baty
+									</h5>
+									<p>
+										<em class="big">
+											I needed more leads for my services. Pay-per-click, banners of maybe even broschures. They made an analysis of my existing site. It occurred that my webdite is banned with Google, and I never knew about that!
+										</em>
+									</p>
+								</div>
+								<div class="quote-item">
+									<div class="quote-image">
+										<img src="images/team/01.jpg" alt="">
+									</div>
+									<p class="small-text author-job">
+										Moving co
+									</p>
+									<h5>
+										Jeffrey P. McAllister
+									</h5>
+									<p>
+										<em class="big">
+											I highly recommend this company for all and any of your design needs. I am very happy with the new redesigned and restructured website they built for my moving company!
+										</em>
+									</p>
+								</div>
+								<div class="quote-item">
+									<div class="quote-image">
+										<img src="images/team/03.jpg" alt="">
+									</div>
+									<p class="small-text author-job">
+										Paradox Inc
+									</p>
+									<h5>
+										Josephine B. Anderson
+									</h5>
+									<p>
+										<em class="big">
+											This guys are awesome! It is hard to find a web design company who can actually listen and understand what you need. I’m 100% satisfied with this guys. My website is exactly what I needed and even more...
+										</em>
+									</p>
+								</div>
+
+							</div>
+							<!-- .testimonials-slider -->
+
+						</div>
+
+						<div class="divider-10 d-none d-lg-block"></div>
+					</div>
+				</div>
+			</section>
+
+<?php include('footer.php');?>
